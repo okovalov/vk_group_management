2a614da4763ce8bfa668df1c18c93e1cd5b67d32
Oleksandr Kovalov <oleksandrk@nationalfibre.net>
2013-03-05 23:49:26 -0500

    Implemented functionality to retrieve a group members count, refactored existed functionality

--- popup.js
+++ popup.js
@@ -1,6 +1,33 @@
 /*global document, chrome, alert, XMLHttpRequest */
 
-var QUERY                = 'kittens';
+function handleError(error) {
+    "use strict";
+
+    var errorMessageElement;
+
+    chrome.storage.local.remove('vk_access_token');
+
+    errorMessageElement = document.createElement('h1');
+    errorMessageElement.textContent = error.error_msg;
+    document.body.appendChild(errorMessageElement);
+}
+
+function serialize(obj, prefix) {
+    "use strict";
+
+    var str = [],
+        p,
+        k,
+        v;
+
+    for (p in obj) {
+        k = prefix ? prefix + "[" + p + "]" : p;
+        v = obj[p];
+        str.push(typeof v === "object" ? serialize(v, k) : encodeURIComponent(k) + "=" + encodeURIComponent(v));
+    }
+
+    return str.join("&");
+}
 
 /**
  * Display an alert with an error message, description
@@ -13,6 +40,35 @@ function displayeAnError(textToShow, errorToShow) {
 
     alert(textToShow + '\n' + errorToShow);
 }
+
+function createHtmlFromString(stringAnswer) {
+    "use strict";
+
+    var doc = document.implementation.createHTMLDocument('example');
+
+    doc.documentElement.innerHTML = stringAnswer;
+
+    return doc;
+}
+
+var vkGlobalAccessToken,
+    VkApi = function () {
+        "use strict";
+
+        var vkURL   = 'https://api.vk.com/method/',
+            request = new XMLHttpRequest();
+
+        this.get = function (methodName, onloadCallaback, callerInstance, incomingParameters, incomingRequestType) {
+            var parameters    = incomingParameters === undefined ? undefined : incomingParameters,
+                requestType   = incomingRequestType === undefined ? 'GET' : incomingRequestType,
+                urlParameters = parameters !== undefined ? '?' + serialize(parameters) : '';
+
+            request.open(requestType,  vkURL + methodName + urlParameters, true);
+            request.onload = onloadCallaback.bind(callerInstance);
+            request.send(null);
+        };
+    },
+    vkApiInstance = new VkApi();
 
 /**
  * Retrieve a value of a parameter from the given URL string
@@ -43,119 +99,149 @@ function getUrlParameterValue(url, parameterName) {
     return parameterValue;
 }
 
-var friendsGenerator = {
+function authenticationListener(authenticationTabId) {
+    "use strict";
 
-    authenticationListener: function (authenticationTabId, requestFriendsInfo, showFriends) {
-        "use strict";
+    return function tabUpdateListener(tabId, changeInfo) {
+        var vkAccessToken,
+            vkAccessTokenExpiredFlag;
 
-        return function tabUpdateListener(tabId, changeInfo) {
-            var vkAccessToken,
-                vkAccessTokenExpiredFlag;
+        if (tabId === authenticationTabId && changeInfo.url !== undefined && changeInfo.status === "loading") {
 
-            if (tabId === authenticationTabId && changeInfo.url !== undefined && changeInfo.status === "loading") {
+            if (changeInfo.url.indexOf('oauth.vk.com/blank.html') > -1) {
+                authenticationTabId = null;
+                chrome.tabs.onUpdated.removeListener(tabUpdateListener);
 
-                if (changeInfo.url.indexOf('oauth.vk.com/blank.html') > -1) {
-                    authenticationTabId = null;
-                    chrome.tabs.onUpdated.removeListener(tabUpdateListener);
+                vkAccessToken = getUrlParameterValue(changeInfo.url, 'access_token');
 
-                    vkAccessToken = getUrlParameterValue(changeInfo.url, 'access_token');
-
-                    if (vkAccessToken === undefined || vkAccessToken.length === undefined) {
-                        displayeAnError('vk auth response problem', 'access_token length = 0 or vkAccessToken == undefined');
-                        return;
-                    }
-
-                    vkAccessTokenExpiredFlag = Number(getUrlParameterValue(changeInfo.url, 'expires_in'));
-
-                    if (vkAccessTokenExpiredFlag !== 0) {
-                        displayeAnError('vk auth response problem', 'vkAccessTokenExpiredFlag != 0' + vkAccessToken);
-                        return;
-                    }
-
-                    chrome.storage.local.set({'vk_access_token': vkAccessToken}, function () {
-                        console.log('just already authorized with', vkAccessToken);
-                        requestFriendsInfo(vkAccessToken, showFriends);
-                    });
+                if (vkAccessToken === undefined || vkAccessToken.length === undefined) {
+                    displayeAnError('vk auth response problem', 'access_token length = 0 or vkAccessToken == undefined');
+                    return;
                 }
-            }
-        };
-    },
 
-    requestAuthentication: function () {
+                vkAccessTokenExpiredFlag = Number(getUrlParameterValue(changeInfo.url, 'expires_in'));
 
-        "use strict";
+                if (vkAccessTokenExpiredFlag !== 0) {
+                    displayeAnError('vk auth response problem', 'vkAccessTokenExpiredFlag != 0' + vkAccessToken);
+                    return;
+                }
 
-        var imageUploadHelperUrl   = 'upload.html#',
-            vkCLientId             = '3470032',
-            authenticationListener = this.authenticationListener,
-            requestFriendsInfo     = this.requestFriendsInfo,
-            showFriends            = this.showFriends,
-            vkRequestedScopes      = 'docs,offline,friends,groups',
-            vkAuthenticationUrl    = 'https://oauth.vk.com/authorize?client_id=' + vkCLientId + '&scope=' + vkRequestedScopes + '&redirect_uri=http%3A%2F%2Foauth.vk.com%2Fblank.html&display=page&response_type=token';
+                chrome.storage.local.set({'vk_access_token': vkAccessToken}, function () {
+                    vkGlobalAccessToken = vkAccessToken;
 
-        chrome.storage.local.get({'vk_access_token': {}}, function (items) {
-
-            if (items.vk_access_token.length === undefined) {
-                chrome.tabs.create({url: vkAuthenticationUrl, selected: true}, function (authenticationTab) {
-                    chrome.tabs.onUpdated.addListener(authenticationListener(authenticationTab.id, requestFriendsInfo, showFriends));
+                    requestFriendsInfo();
                 });
-
-                return;
             }
+        }
+    };
+}
 
-            requestFriendsInfo(items.vk_access_token, showFriends);
-        });
-    },
+function requestAuthentication() {
+    "use strict";
 
-    showFriends: function (e) {
-        "use strict";
+    var vkAuthenticationUrl      = 'https://oauth.vk.com/authorize',
+        vkCLientId               = '3470032',
+        vkRequestedScopes        = 'docs,offline,friends,groups',
+        authenticationParameters = {
+            'client_id'     : vkCLientId,
+            'scope'         : vkRequestedScopes,
+            'redirect_uri'  : encodeURIComponent("http://oauth.vk.com/blank.html"),
+            'display'       : 'page',
+            'response_type' : 'token'
+        },
+        urlParameters = '?' + serialize(authenticationParameters);
 
-        var i,
-            friends,
-            errorMessage,
-            answer = JSON.parse(e.target.response),
-            friendsList,
-            friendItem;
+    vkAuthenticationUrl += urlParameters;
 
-        if (answer.error !== undefined) {
-            chrome.storage.local.remove('vk_access_token');
+    if (vkGlobalAccessToken !== undefined) {
+        requestFriendsInfo();
+    }
 
-            errorMessage = document.createElement('h1');
-            errorMessage.textContent = answer.error.error_msg;
-            document.body.appendChild(errorMessage);
+    chrome.storage.local.get({'vk_access_token': {}}, function (items) {
+
+        if (items.vk_access_token.length === undefined) {
+            chrome.tabs.create({url: vkAuthenticationUrl, selected: true}, function (authenticationTab) {
+                chrome.tabs.onUpdated.addListener(authenticationListener(authenticationTab.id));
+            });
 
             return;
         }
 
-        friends = answer.response;
+        vkGlobalAccessToken = items.vk_access_token;
 
-        friendsList = document.createElement('ul');
+        requestFriendsInfo();
+    });
+}
 
-        document.body.appendChild(friendsList);
+function showFriends(e) {
+    "use strict";
 
-        for (i = 0; i < friends.length; i += 1) {
-            friendItem = document.createElement('li');
-            friendItem.innerHTML = friends[i].first_name + ' ' + friends[i].last_name;
-            friendsList.appendChild(friendItem);
+    var answer = JSON.parse(e.target.response),
+        friends,
+        friendsInfo;
 
-        }
-    },
+    if (answer.error !== undefined) {
+        handleError(answer.error);
 
-    requestFriendsInfo: function (vkAccessToken, showFriends) {
-        "use strict";
-
-        var req = new XMLHttpRequest();
-
-        req.open("GET", 'https://api.vk.com/method/friends.get?access_token=' + vkAccessToken + '&fields=uid,first_name,last_name', true);
-        req.onload = showFriends.bind(this);
-        req.send(null);
+        return;
     }
 
-};
+    friends = answer.response;
 
+    friendsInfo           = document.createElement('p');
+    friendsInfo.innerHTML = 'Friends count: ' + friends.length;
+
+    document.body.appendChild(friendsInfo);
+
+    requestGroupMembersList();
+}
+
+function requestFriendsInfo(friendsGenerator) {
+    "use strict";
+
+    var parameters = {
+            'access_token' : vkGlobalAccessToken,
+            'fields'       : 'uid,first_name,last_name'
+        };
+
+    vkApiInstance.get('friends.get', showFriends, this, parameters);
+}
+
+function showMembers(e) {
+    "use strict";
+
+    var answer = JSON.parse(e.target.response),
+        members,
+        membersInfo;
+
+    if (answer.error !== undefined) {
+        handleError(answer.error);
+
+        return;
+    }
+
+    members = answer.response;
+
+    membersInfo           = document.createElement('p');
+    membersInfo.innerHTML = 'Group members count: ' + members.count;
+
+    document.body.appendChild(membersInfo);
+}
+
+function requestGroupMembersList() {
+    "use strict";
+
+    var parameters = {
+            'access_token' : vkGlobalAccessToken,
+            'gid'          : 49912690,
+            'sort'         : 'id_asc'
+        };
+
+    vkApiInstance.get('groups.getMembers', showMembers, this, parameters);
+}
 
 document.addEventListener('DOMContentLoaded', function () {
     "use strict";
 
-    friendsGenerator.requestAuthentication();
+    requestAuthentication();
 });

db6138353d20368181fde0a14a9b45bdc050d426
okovalov <tradakad@gmail.com>
2013-03-23 22:22:28 -0400

    Minor code standards fixes

--- background.js
+++ background.js
@@ -1,10 +1,9 @@
 /*global require, chrome, alert */
 
+chrome.browserAction.onClicked.addListener(function () {
+    "use strict";
 
-
-chrome.browserAction.onClicked.addListener(function() {
-
-    chrome.tabs.create({url: "loading.html"}, function(tab) {
+    chrome.tabs.create({url: "loading.html"}, function (tab) {
         requestAuthentication(tab.id);
     });
 });
